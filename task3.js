99f39c9b798251219ab2eb5b39a073b4daa5c8c3
ForceAwakenz <djimeilsobachka@gmail.com>
2021-07-02 01:19:27 +0300

    minor grammar corrects

--- task3.js
+++ task3.js
@@ -3,12 +3,12 @@
 
 function ifArraySorted(inputArray) {
     
-    if (!Array.isArray(inputArray)) { return "Invalid data, insert array" };
+    if (!Array.isArray(inputArray)) { return "Invalid data, insert an array" };
     let ifSorted;
-    if (isNaN(inputArray[0])) return "Error, array must contain only numbers";
+    if (isNaN(inputArray[0])) return "Error, an array must contain only numbers";
 
     for (let i = 1; i < inputArray.length; i++) {
-        if (isNaN(inputArray[i])) return "Error, array must contain only numbers";
+        if (isNaN(inputArray[i])) return "Error, an array must contain only numbers";
         if (inputArray[i - 1] <= inputArray[i]) {
             ifSorted = "An array is sorted";
         } else { return "An array is not sorted"; }

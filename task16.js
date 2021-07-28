94784f67821ea5da0e07ecdd176a998113a25164
ForceAwakenz <djimeilsobachka@gmail.com>
2021-07-29 00:12:04 +0300

    improved task16 solution

--- task16.js
+++ task16.js
@@ -1,8 +1,14 @@
 'use strict'
 // 3. Написать функцию-обертку, замеряющую и выводящую в консоль время выполнения синхронной функции, переданной в функцию-обертку в качестве коллбека. Для замера можно использовать performance.now(); .
 
-function multiplyByThousand(number) {
-    return number * 1000;
+function someComplexStuff(number) {
+    return String(number)
+        .split('')
+        .filter(num => +num % 2 === 0 && +num !== 0)
+        .reduce((total, current) => total + current, '')
+        .split('')
+        .map(num => num ** 10)
+        .reduce((total, current) => total + +current, 0)
 };
 
 
@@ -13,8 +19,8 @@ function wrapper(callback) {
     
     const finish = performance.now();
 
-    return ((finish - start)).toFixed(2);
+    return finish - start;
 
 };
 
-console.log(wrapper(() => multiplyByThousand(565805840)));
+console.log(wrapper(() => someComplexStuff(56582004395265356582004395n)));
